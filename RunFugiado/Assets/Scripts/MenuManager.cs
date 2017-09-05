dd62b938ab6aefefee4d9d98f9a3a3e06aeffab3
JackFury360 <Gabriel.Vieira@cejoseleitelopes.com.br>
2017-09-05 14:58:06 -0300

    Menu

---
+++ RunFugiado/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
