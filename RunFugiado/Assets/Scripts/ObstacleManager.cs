fa3ba35ae49dce6210607e474d297a04bbe22739
JackFury360 <gabrielfigueiredov@gmail.com>
2017-09-13 12:13:28 -0300

    box

--- RunFugiado/Assets/Scripts/ObstacleManager.cs
+++ RunFugiado/Assets/Scripts/ObstacleManager.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour {
-
+    public float Vel;
 	// Use this for initialization
 	void Start () {
-		
+        Vel = GameObject.Find("Bg").GetComponent<Parallax>().parallaxVel;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.Translate(-Vel * Time.deltaTime, 0, 0);
+    }
 }
---
+++ RunFugiado/Assets/Scripts/ObstacleSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour {
+    public GameObject box;
+    public float boxPosX;
+    public float boxPosY;
+
+    public float TimerBox;
+	// Use this for initialization
+	void Start () {
+        boxPosX = 2f;
+        boxPosY = -3.33f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimerBox += Time.deltaTime;
+
+        if(TimerBox > 1.5f)
+        {
+            GameObject boxObstacle = Instantiate(box) as GameObject;
+            boxObstacle.transform.position = new Vector3(boxPosX , boxPosY, 0);
+            TimerBox = 0;
+        }
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Bateu");
+        }
+    }
+}
