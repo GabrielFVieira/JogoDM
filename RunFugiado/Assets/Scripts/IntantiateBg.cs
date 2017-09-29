b05cf11605fb644a1991edef89643d728edf648f
JackFury360 <gabrielfigueiredov@gmail.com>
2017-09-29 14:01:54 -0300

    Parallax Fixed

---
+++ RunFugiado/Assets/Scripts/IntantiateBg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntantiateBg : MonoBehaviour {
+    public GameObject posX;
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(posX.transform.position.x + posX.GetComponent<SpriteRenderer>().bounds.size.x, transform.position.y, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
