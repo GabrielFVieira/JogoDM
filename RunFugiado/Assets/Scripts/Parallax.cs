36dac5b12d7ddc11091fbc3fe27da8153d708aa2
JackFury360 <gabrielfigueiredov@gmail.com>
2017-10-03 18:19:01 -0300

    Starting to add break anim

--- RunFugiado/Assets/Scripts/Parallax.cs
+++ RunFugiado/Assets/Scripts/Parallax.cs
@@ -14,30 +14,9 @@ public class Parallax : MonoBehaviour {
     public Sprite[] bg;
 
     public LevelManager manager;
-<<<<<<< HEAD
-	// Use this for initialization
-	void Start () {
-        /*minX = -27.36997f;
-        startX = 10.67622f;*/
-=======
-    /*
-    private double verticalSize;
-    private double horizontalSize;
-    public float cameraOffset;*/
 
-    // Use this for initialization
     void Start () {
-        minX = -27.36997f;
-        startX = 10.67622f;
 
-        /*
-        verticalSize = Camera.main.orthographicSize * 2.0;
-        horizontalSize = verticalSize * Screen.width / Screen.height;        
-
->>>>>>> f52415e431c73352e1e6ad5deb11f0701dba4b58
-        cam = GameObject.FindGameObjectWithTag("MainCamera");
-        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelManager>();
-        cameraOffset = cam.transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x);*/
     }
 	
 	// Update is called once per frame
@@ -47,28 +26,12 @@ public class Parallax : MonoBehaviour {
         if (GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().GetBool("Die") == false)
         {
             // METODO DAYVID 
-            if (transform.position.x < Mathf.Round(minX))
+            /*if (transform.position.x < Mathf.Round(minX))
             {
                 transform.position = new Vector3(Mathf.Round(startX), 0, 0);
-            }
+            }*/
 
             transform.Translate(new Vector3(-parallaxVel, 0, 0) * Time.deltaTime);
-
-            /*   transform.Translate(-parallaxVel * Time.deltaTime, 0, 0);
-
-               if (transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x / 2) < cam.transform.position.x - (horizontalSize / 2))
-               {                
-                   transform.position = new Vector3(cameraOffset, transform.position.y, transform.position.z);
-                   Debug.Log(transform.position.x);
-               }   */
-
-
-            /*
-            if (transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x / 2) < cam.transform.position.x - (GetComponent<SpriteRenderer>().bounds.size.x / 2))
-            {
-                transform.position = new Vector3(cam.transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x), transform.position.y, transform.position.z);
-            }*/
-<<<<<<< HEAD
 
             if (transform.localPosition.x < minX)
             {
@@ -83,15 +46,8 @@ public class Parallax : MonoBehaviour {
 
                 transform.localPosition = new Vector3(startX, 4.33f);
             }
-            
-            if(manager.gameTimer >=  20 && manager.gameTimer < 40)
-=======
-            /*
-            if (transform.position.x < minX)
-                transform.position = new Vector3(startX, transform.position.y);
-            */
-            if (manager.gameTimer >= 20 && manager.gameTimer < 40)
->>>>>>> f52415e431c73352e1e6ad5deb11f0701dba4b58
+               
+                if (manager.gameTimer >= 20 && manager.gameTimer < 40)
                 parallaxVel = 5.8f;
 
             if (manager.gameTimer >= 40 && manager.gameTimer < 60)
